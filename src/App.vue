d20d4d6edf0da9bf200a491f64c6cdbaffa8efa4
Patrol200611 <patrol200611@ukr.net>
2024-06-16 14:07:30 +0300

    my vue project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+// Импортируем компонент HelloWorld.vue
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld // Регистрируем компонент в опциях компонента
+  }
+}
+</script>
+
+<template class="grid">
+  <h1 class="text-3xl text-center font-bold underline">Hello world!</h1>
+  <div class="text-center width-300 ">
+    <HelloWorld />
+    <p>Lorem, ipsum.</p>
+  </div>
+  <h2 class="text-right">New News</h2>
+</template>
+
+<style>
+body {
+  background-color: rgb(224, 8, 66);
+}
+</style>
