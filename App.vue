fbe721c4e2fce18b366f604f098fa481212152ab
Wlad <patrol200611@ukr.net>
2024-07-06 16:44:06 +0300

    lesson №4

---
+++ App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+const navItems = ['timeline', 'activities', 'progress']
+</script>
+<template>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a href="#">
+      <img src="./src/assets/logo.png" alt="logo" class="h-7" />
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1">
+        <!-- <div v-if="true" class="flex items-center gap-1"> -->
+        Day complete!
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+      <div v-else class="flex items-center gap-1">
+        <div>
+          Progress:
+          <span class="font-mono">20%</span>
+        </div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+    </a>
+  </header>
+  <main class="flex flex-grow flex-col"></main>
+  <nav class="sticky bottom-0 z-10 border-b bg-gray-200 text-xl">
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="page in navItems" :key="page" class="flex-1">
+        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize"
+          ><ClockIcon v-if="page==='timeline'" class="h-7 w-7" />
+          <ListBulletIcon v-else-if="page==='activities'" class="h-7 w-7" />
+          <ChartBarIcon v-else class="h-7 w-7" />{{ page }}</a
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+<style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./app.vue"
   ],
   theme: {
     extend: {},
