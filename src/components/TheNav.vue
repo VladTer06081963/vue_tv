e52c6f897e7b5ed95b3219e8778caedf7af854da
Wlad <patrol200611@ukr.net>
2024-07-07 12:47:36 +0300

    переводим массив в обьект

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
-const navItems = ['timeline', 'activities', 'progress']
+const navItems = {
+  timeline: ClockIcon,
+  activities: ListBulletIcon,
+  progress: ChartBarIcon
+}
 </script>
 
 <template>
-<nav class="sticky bottom-0 z-10 border-b bg-gray-200 text-xl">
+  <nav class="sticky bottom-0 z-10 border-b bg-gray-200 text-xl">
     <ul class="flex items-center justify-around border-t">
-      <li v-for="page in navItems" :key="page" class="flex-1">
+      <li v-for="(icon, page) in navItems" :key="page" class="flex-1">
         <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize"
-          ><ClockIcon v-if="page === 'timeline'" class="h-7 w-7" />
-          <ListBulletIcon v-else-if="page === 'activities'" class="h-7 w-7" />
-          <ChartBarIcon v-else class="h-7 w-7" />{{ page }}</a
+          ><component :is="icon" class="h-7 w-7" />{{ page }}</a
         >
       </li>
     </ul>
